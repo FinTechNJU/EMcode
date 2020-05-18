43ea0099be341fa61b5d5e5d19d76f4d5097bf52
FinTech_NJU <fintech_nju@foxmail.com>
2020-05-18 16:15:12 +0800

    本机运行 pw=13人 sc=15省 conv=10

--- IT实习生_2/bakcup/WindFunds/DataBackUp/第二轮 第三天/process3.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 13 00:18:38 2020
-
-@author: hs101
-"""
-
-import pandas as pd
-dataRaw = pd.read_csv("stocks.csv", encoding = 'gbk')
--- 本机运行/MatLab/正在运行/mainprogram.m
+++ 本机运行/MatLab/正在运行/mainprogram.m
@@ -28,7 +28,7 @@ Aeq = [zeros(1,N),ones(1,K),zeros(1,K+4+2)];  beq = 1;
 Ybar = 50;
 
 % set up convergence criterion for the EM algorithm
-convergcrit = 5;
+convergcrit = 1;
 
 
 %% Part 2: execution of the main EM algorithm
